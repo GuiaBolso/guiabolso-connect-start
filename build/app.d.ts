4cde3b11015b70da3d1df568626f72a3e9bad78d
Henrique Moreira <henryqrm@gmail.com>
2021-05-20 10:20:57 -0300

    fix: 🐛 changes package

---
+++ build/app.d.ts
@@ -0,0 +1,69 @@
+declare const events: readonly ["load", "onboard", "signup", "bank_list", "bank_not_found", "bank_selected", "bank_offline", "synced", "complete", "back", "exit", "error"];
+type EventsConnectKeys = typeof events[number];
+type RenderIframeParams = {
+    container?: HTMLElement;
+    src: string;
+    config?: {
+        height?: number | string;
+        width?: number | string;
+    };
+};
+type RenderButtonParams = {
+    container?: HTMLElement;
+    onClick(event: ElementEventMap['fullscreenchange']): any;
+    config?: {
+        allowIcon?: boolean;
+        label?: string;
+    };
+};
+type Envs = 'production' | 'sandbox';
+export type GuiabolsoConnectParams = {
+    config?: Partial<{
+        hiddenHeader: boolean;
+        labelShareButton: string;
+        iframe: RenderIframeParams['config'];
+        window: RenderIframeParams['config'];
+        button: RenderButtonParams['config'];
+    }>;
+    data: {
+        cpf: string;
+        email: string;
+        phone: string;
+    };
+    environment?: Envs;
+    userTrackingId: string;
+    clientId: string;
+    callbackURL: string;
+    /**
+     * only when open a new window
+     */
+    fallbackURL?: string;
+    /**
+     * DOM element to render iframe or button
+     */
+    container: HTMLElement;
+};
+export type CallbackPayload<K extends EventsConnectKeys> = {
+    load(): void;
+    onboard(): void;
+    signup(): void;
+    bank_list(): void;
+    bank_not_found(): void;
+    bank_selected(): void;
+    bank_offline(): void;
+    synced(): void;
+    complete(params: {
+        oauthcode: string;
+    }): void;
+    back(): void;
+    exit(params: {
+        reason: 'back_finished' | 'bank_not_found' | 'unknow' | 'user_cancel';
+    }): void;
+    error(err: Error): void;
+}[K];
+export function guiabolsoConnect({ container, config, data, clientId, userTrackingId, callbackURL, fallbackURL, environment, }: GuiabolsoConnectParams): Promise<{
+    on: <T extends "error" | "exit" | "complete" | "load" | "onboard" | "signup" | "bank_list" | "bank_not_found" | "bank_selected" | "bank_offline" | "synced" | "back">(event: T, cb: CallbackPayload<T>) => void;
+    openNewWindow: () => void;
+    events: readonly ["load", "onboard", "signup", "bank_list", "bank_not_found", "bank_selected", "bank_offline", "synced", "complete", "back", "exit", "error"];
+    destroy: () => void;
+}>;
